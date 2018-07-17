acd9e5b80179f15afe69697c1a8c67ee0a51d062
XuYibo <xuyibo@outlook.com>
2018-07-17 20:03:49 +0800

    添加默认用户

--- domain/src/main/java/com/yibo/utils/TokenUtil.java
+++ domain/src/main/java/com/yibo/utils/TokenUtil.java
@@ -9,6 +9,9 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 public class TokenUtil {
     public static SysUser getUser(HttpServletRequest request) {
         String header = request.getHeader(AUTHORIZATION);
+        if (null == header) {
+            return new SysUser(0L, "system");
+        }
         int index = header.indexOf(",");
         Long id = Long.parseLong(header.substring(0, index));
         String username = header.substring(index + 1, header.length());
