4737675a32ed18e3b66edfbb14328e3a40de9694
Yibo <xuyibo@outlook.com>
2018-07-20 10:39:04 +0800

    添加admin监控模块

---
+++ admin/src/main/java/com/yibo/admin/AdminApplication.java
@@ -0,0 +1,15 @@
+package com.yibo.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableAdminServer
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
